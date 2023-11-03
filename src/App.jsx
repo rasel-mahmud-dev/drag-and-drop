ea422c5fad4705f1dfeceefb235d23fef6d2bfaf
rasel-mahmud-dev <rasel.mahmud.dev@gmail.com>
2023-11-03 10:58:39 +0600

    update 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,68 @@
-import {useRef, useState} from "react"
-import "./style.css"
+import { useRef, useState } from "react";
+import "./style.css";
 
 function App() {
-
-    const dragItem = useRef()
-
+    const dragItem = useRef();
     const [items, setItems] = useState([
-        {id: 1, title: "sdf"},
-        {id: 2, title: "sdf"},
-        {id: 3, title: "sdf"},
-        {id: 4, title: "sdf"},
-        {id: 5, title: "sdf"},
-        {id: 6, title: "sdf"},
-        {id: 7, title: "sdf"},
-        {id: 8, title: "sdf"}
-    ])
+        { id: 1, title: "item 1" },
+        { id: 2, title: "item 2" },
+        { id: 3, title: "item 3" },
+        { id: 4, title: "item 4" },
+        { id: 5, title: "item 5" },
+        { id: 6, title: "item 6" },
+        { id: 7, title: "item 7" },
+        { id: 8, title: "item 8" }
+    ]);
+    const [dragging, setDragging] = useState(null); // Added dragging state
 
-    function handleDragStart(e) {
-        dragItem.current = e.target
-        e.target.classList.add("drag-item")
+    function handleDragStart(e, id) {
+        dragItem.current = id;
+        setDragging(id); // Set the currently dragging item
+        e.dataTransfer.effectAllowed = "move";
     }
 
     function handleDragOver(e) {
-        console.log("over", e.target)
+        e.preventDefault();
     }
 
+    function handleDrop(e, id) {
+        e.preventDefault();
 
-    function handleDragEnd(e) {
-        console.log("end", e.target)
-        e.target.classList.remove("drag-item")
+        const dragItemId = dragItem.current;
+        const updatedItems = [...items];
+
+        const draggedItemIndex = updatedItems.findIndex((item) => item.id === dragItemId);
+        const dropTargetIndex = updatedItems.findIndex((item) => item.id === id);
+
+        const draggedItem = updatedItems[draggedItemIndex];
+        updatedItems.splice(draggedItemIndex, 1);
+        updatedItems.splice(dropTargetIndex, 0, draggedItem);
+
+        setItems(updatedItems);
+        setDragging(null); // Reset the currently dragging item after dropping
     }
-
 
     return (
         <div>
-            <div className="gellery">
-                {items.map(item => (
-                   <div className="box-root">
-                       <div
-                           onDragEnd={handleDragEnd}
-                           onDragOver={handleDragOver}
-                           onDragStart={handleDragStart}
-                           draggable={true}
-                           className={`box box-${item.id}`}>
-                           {item.id}
-                       </div>
-                   </div>
+            <div className="gallery">
+                {items.map((item) => (
+                    <div key={item.id} className="box-root">
+                        <div key={item.id} className="box-root">
+                            <div
+                                onDragStart={(e) => handleDragStart(e, item.id)}
+                                onDragOver={handleDragOver}
+                                onDrop={(e) => handleDrop(e, item.id)}
+                                draggable={true}
+                                className={`box box-${item.id} ${dragging === item.id ? "dragging" : ""}`}
+                            >
+                                {item.title}
+                            </div>
+                        </div>
+                    </div>
                 ))}
             </div>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
